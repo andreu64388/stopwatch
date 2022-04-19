13f995d173d6092ab3e2a2c1f518330b4bb633d1
Andr_15_sh <90029729+andreu64388@users.noreply.github.com>
2022-04-20 01:42:56 +0300

    .

--- src/componets/Timer.tsx
+++ src/componets/Timer.tsx
@@ -1,66 +1,86 @@
-import React, { ChangeEvent, FC, useState } from 'react'
+import React, { ChangeEvent, FC, useEffect, useState } from 'react'
 import { formatTime } from '../functions/Time'
-
 const Timer: FC = () => {
    const [hours, setHours] = useState<string>("00")
    const [minutes, setMinutes] = useState<string>("00")
-   const [seconds, setSeconds] = useState<string>("42")
+   const [seconds, setSeconds] = useState<string>("22")
    const [time, setTime] = useState({ s: 0, m: 0, h: 0 })
-   const [isActive, setIsActive] = useState(0)
+   const [isActive, setIsActive] = useState<boolean>(false)
    const [inter, setInter] = useState<any>()
-   const [isStart, setIsStart] = useState(false)
    const timeFull = `${formatTime(time.h)}:${formatTime(time.m)}:${formatTime(time.s)}`;
+   useEffect(() => {
+      setTime({ s: Number(seconds), m: Number(minutes), h: Number(hours) })
 
+   }, [hours, minutes, seconds])
+   var inters:any = null;
    const Start = () => {
-      setTime({ s: Number(hours), m: Number(minutes), h: Number(hours) })
-      setInter(setInterval(Run, 1000))
+
+      setIsActive(true)
+      inters = (setInterval(Countdown, 1000))
+
+   }
+   const Stop = () => {
+      setTime({ s: Number(seconds), m: Number(minutes), h: Number(hours) })
+      setIsActive(false)
+
    }
    let updatedH: number = time.h;
    let updatedM: number = time.m;
    let updatedS: number = time.s;
 
-   const Run = () => {
-      if (updatedS === 0) {
+   const Countdown = () => {
+      if
+         (updatedS === 0) {
          updatedM--;
          updatedS = 59;
-      }
-      if (updatedM) {
+      } else {
          updatedS--;
-         updatedS = 59;
+      }
+      if (updatedM < 0) {
+         updatedH--;
+         updatedM = 59;
       }
 
-      updatedS--;
+      if (updatedH === 0 && updatedM === 0 && updatedS === 0) {
+
+         setIsActive(false)
+         clearInterval(inters)
+         updatedH = time.h
+         updatedM = time.m;
+         updatedS = time.s;
+      }
+
       setTime({ s: updatedS, m: updatedM, h: updatedH })
    }
 
-
    return (
       <div className='timer'>
-         {timeFull}
 
          <div className="timer_main">
             <div className="cirsycle">
-
-
                <div className='timer_input'>
-                  <input type="text"
-                     maxLength={2}
+                  {isActive ? <h1>{timeFull}</h1> : <>
+                     <input type="text"
+                        maxLength={2}
+                        value={hours} onChange={(e: ChangeEvent<HTMLInputElement>) => setHours(e.target.value)}
+                     />:
+                     <input type="text" value={minutes}
+                        maxLength={2}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => setMinutes(e.target.value)} />:
 
-                     value={hours} onChange={(e: ChangeEvent<HTMLInputElement>) => setHours(e.target.value)}
-                  />:
-                  <input type="text" value={minutes}
-                     maxLength={2}
-                     onChange={(e: ChangeEvent<HTMLInputElement>) => setMinutes(e.target.value)} />:
-                  :
-                  <input type="text" value={seconds}
-                     maxLength={2}
-                     onChange={(e: ChangeEvent<HTMLInputElement>) => setSeconds(e.target.value)}
-                  />
+                     <input type="text" value={seconds}
+                        maxLength={2}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => setSeconds(e.target.value)}
+                     /></>
+                  }
+
                </div>
             </div>
          </div>
          <div className="btns_timer">
-            <button onClick={Start}>Start</button>
+
+            {isActive ? <button onClick={Stop}>Stop</button> : <button onClick={Start}>Start</button>}
+
          </div>
 
       </div>
